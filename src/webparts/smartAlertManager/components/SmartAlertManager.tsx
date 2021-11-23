552710f31e741648c37dc729a1e27c23d826dd57
Russell gove <russellgove@gmail.com>
2021-11-23 13:55:34 -0500

    showing alerts

--- src/webparts/smartAlertManager/components/SmartAlertManager.tsx
+++ src/webparts/smartAlertManager/components/SmartAlertManager.tsx
@@ -1,9 +1,12 @@
+import { PrimaryButton } from '@microsoft/office-ui-fabric-react-bundle';
 import { escape } from '@microsoft/sp-lodash-subset';
 import { sp } from '@pnp/sp';
 import { IListInfo, ILists, Lists } from "@pnp/sp/lists";
 import { ISubscription, ISubscriptions, Subscriptions } from "@pnp/sp/subscriptions";
 import { IWebs, Webs } from "@pnp/sp/webs";
 import { IViewField, ListView } from '@pnp/spfx-controls-react/lib/controls/listView';
+import { getIconClassName } from '@uifabric/styling';
+import { filter } from 'lodash';
 import { IColumn } from 'office-ui-fabric-react/lib/components/DetailsList';
 import { DatePicker } from 'office-ui-fabric-react/lib/DatePicker';
 import { Panel, PanelType } from 'office-ui-fabric-react/lib/Panel';
@@ -19,28 +22,60 @@ import "@pnp/sp/webs";
 
 export const SmartAlertManager: React.FunctionComponent<ISmartAlertManagerProps> = (props) => {
   const [listInfos, setListInfos] = useState<IListInfo[]>();
-  const viewFieldsCallbackItems: any[] = [
-    //{
-    //   name: 'actions', minWidth: 50, maxWidth: 50, displayName: 'Actions', render: (item?: any, index?: number) => {
-    //     return <div>
-    //       <i className={getIconClassName('Redo')}
-    //         onClick={async (e) => {
-    //           debugger;
-    //           const url = `${parentContext.managementApiUrl}/api/EnqueueCallbackItems`;
-    //           const selected = [item];
-    //           await fetchAZFunc(parentContext.aadHttpClient, url, "POST", JSON.stringify(selected));
-    //           alert(`${selected.length} files where queued`);
-    //         }}></i>
-    //       &nbsp;&nbsp;    &nbsp;&nbsp;    &nbsp;&nbsp;
-    //       <i className={getIconClassName('View')} onClick={(e) => {
-    //         debugger;
-    //         setSelectedCallbackItem(item);
-    //         setMode("showselected");
+  const [selectedList, setSelectedList] = useState<IListInfo>();
+  const [mode, setMode] = useState<string>('');
+  const viewFieldsSubscriptions: any[] = [
+    {
+      name: 'actions', minWidth: 50, maxWidth: 50, displayName: 'Actions', render: (item?: IListInfo, index?: number) => {
+        return <div>
+          {/* <i className={getIconClassName('Redo')}
+            onClick={async (e) => {
+              debugger;
+              const url = `${parentContext.managementApiUrl}/api/EnqueueCallbackItems`;
+              const selected = [item];
+              await fetchAZFunc(parentContext.aadHttpClient, url, "POST", JSON.stringify(selected));
+              alert(`${selected.length} files where queued`);
+            }}></i>
+          &nbsp;&nbsp;    &nbsp;&nbsp;    &nbsp;&nbsp; */}
+          <i className={getIconClassName('View')} onClick={(x) => {
+            debugger;
+            // setSelectedList(filter(listInfos,(li)=>{return li.Id===item.Id})[0]);
+            // setMode("showselected");
 
-    //       }}></i>
-    //     </div>;
-    //   }
-    // },
+          }}></i>
+        </div>;
+      }
+    },
+    { name: 'notificationUrl', minWidth: 100, maxWidth: 200, displayName: 'notificationUrl', sorting: true, isResizable: true },
+
+    { name: 'expirationDateTime', minWidth: 80, maxWidth: 120, displayName: 'Expires', sorting: true, isResizable: true },
+    { name: 'id', minWidth: 80, maxWidth: 120, displayName: 'ID', sorting: true, isResizable: true },
+    { name: 'resource', minWidth: 80, maxWidth: 120, displayName: 'Resource', sorting: true, isResizable: true },
+    { name: 'clientState', minWidth: 80, maxWidth: 120, displayName: 'CLientSTate', sorting: true, isResizable: true },
+    
+  ];
+  const viewFieldsCallbackItems: any[] = [
+    {
+      name: 'actions', minWidth: 50, maxWidth: 50, displayName: 'Actions', render: (item?: any, index?: number) => {
+        return <div>
+          {/* <i className={getIconClassName('Redo')}
+            onClick={async (e) => {
+              debugger;
+              const url = `${parentContext.managementApiUrl}/api/EnqueueCallbackItems`;
+              const selected = [item];
+              await fetchAZFunc(parentContext.aadHttpClient, url, "POST", JSON.stringify(selected));
+              alert(`${selected.length} files where queued`);
+            }}></i>
+          &nbsp;&nbsp;    &nbsp;&nbsp;    &nbsp;&nbsp; */}
+          <i className={getIconClassName('View')} onClick={(x) => {
+            debugger;
+            setSelectedList(filter(listInfos,(li)=>{return li.Id===item.Id})[0]);
+            setMode("showselected");
+
+          }}></i>
+        </div>;
+      }
+    },
     { name: 'Title', minWidth: 100, maxWidth: 200, displayName: 'Title', sorting: true, isResizable: true },
 
     { name: 'Description', minWidth: 80, maxWidth: 120, displayName: 'Description', sorting: true, isResizable: true },
@@ -79,8 +114,26 @@ export const SmartAlertManager: React.FunctionComponent<ISmartAlertManagerProps>
     });
 
   }, []);
+  debugger;
   return <div>
     <ListView items={listInfos} viewFields={viewFieldsCallbackItems}></ListView>
-
+    {mode==="showselected" &&
+    <Panel type={PanelType.extraLarge}
+        headerText={`Smart Alerts for ${selectedList.Title}`}
+        isOpen={mode === "showselected"}
+        onDismiss={(e) => {
+          setMode("Display");
+        }} >
+        <ListView
+          items={filter(listInfos,(li)=>{
+            debugger;
+            return li.Id===selectedList.Id;
+          })[0]['Subscriptions']}
+          viewFields={viewFieldsSubscriptions}
+        //  stickyHeader={true}
+        ></ListView>
+      <PrimaryButton>Add Smart Alert</PrimaryButton>
+      </Panel>
+}
   </div>;
 };
